d0930900340a0e42b149322099f8e98f90a5ca49
Gregorio Jalomo <jalomo@hotmail.es>
2017-09-15 13:21:05 -0500

    index

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const port = process.env.PORT|| 3000
 http.createServer(function(peticion, respuesta){
    respuesta.writeHead(200, 'text/plain');
    respuesta.end('Hola mundo.');
-}).listen( port  , '127.0.0.1');
+}).listen( port );
 console.log('El servidor esta funcionando correctamente en https://localhost:3000/');
