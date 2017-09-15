64627ed12463d06bb64eb1a9f6c044d9381099e6
Gregorio Jalomo <jalomo@hotmail.es>
2017-09-15 11:20:48 -0500

    respaldo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+const port = process.env.PORT|| 3000
 http.createServer(function(peticion, respuesta){
    respuesta.writeHead(200, 'text/plain');
    respuesta.end('Hola mundo.');
-}).listen(3000, '127.0.0.1');
+}).listen( port  , '127.0.0.1');
 console.log('El servidor esta funcionando correctamente en https://localhost:3000/');
